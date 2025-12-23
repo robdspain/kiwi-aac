af723b374b468ead3dabd99e15fb36985f6fd9e4
Rob <rob@Robs-Mac-mini.local>
2025-12-23 10:54:19 -0800

    chore: PWA compliance improvements

--- src/main.jsx
+++ src/main.jsx
@@ -15,6 +15,10 @@ createRoot(document.getElementById('root')).render(
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js');
+    navigator.serviceWorker.register('/sw.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
   });
 }
