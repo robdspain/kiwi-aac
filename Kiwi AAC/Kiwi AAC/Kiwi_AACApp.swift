1a3645c0b70c38cacba2f753905d56f4a80d5504
Rob <rob@Robs-Mac-mini.local>
2025-12-23 13:35:30 -0800

    style: shrink phone frames and fix screenshot alignment in generator and landing page

---
+++ Kiwi AAC/Kiwi AAC/Kiwi_AACApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kiwi_AACApp.swift
+//  Kiwi AAC
+//
+//  Created by Rob on 12/23/25.
+//
+
+import SwiftUI
+
+@main
+struct Kiwi_AACApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
